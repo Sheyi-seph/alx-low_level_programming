acc956bee56d791b43aa59e59de50ee392a1dc51
Sheyi-seph <josepholumayor@gmail.com>
2023-02-16 18:57:46 -0800

    dem no born betty well na me be her papa

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main fumction
+ *
+ * Return: returns 0 when successful compile
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
